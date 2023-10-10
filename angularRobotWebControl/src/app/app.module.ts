c0ead014debe541e04ff38386938190f6d1baadb
Senhor das Galáxias <rssoares@atech.com.br>
2023-10-10 17:50:37 -0300

    angularRobotWebControl - retirando usuario e senha do repositorio e adicionando um environments

--- angularRobotWebControl/src/app/app.module.ts
+++ angularRobotWebControl/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
